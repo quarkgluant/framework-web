4d7a8189efd886af9936807d05777855d7ece24b
Patrice Cadiot <pathibul.r@gmail.com>
2017-10-28 13:42:17 +0200

    add Gemdile

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# Gemfile
+source "https://rubygems.org"
+ruby "2.3.4"
+
+gem 'rack'
+gem 'awesome_print'
+gem 'puma'
+gem 'sqlite3'
+
---
+++ lib/application.rb
@@ -0,0 +1,29 @@
+require 'yaml'
+require 'awesome_print'
+require_relative 'routes_builder'
+require_relative 'renderer'
+require_relative 'base_controller'
+Dir.glob("controller/*.rb") { |controller| require_relative "../#{controller}" }
+
+class Application
+  
+    def initialize
+      builder = RoutesBuilder.new(YAML.load_file("routes.yml"))
+      builder.build
+      @routes = builder.routes
+    end
+
+    def call(env)
+      # ap env
+      route = @routes[env["REQUEST_PATH"]]
+      if route
+        controller = Object.const_get(route['controller']).new
+        controller.send(route['method'])
+      else
+        fail "No matching routes"
+      end
+      
+    end
+
+end
+  
