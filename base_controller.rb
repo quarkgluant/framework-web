58fcc9fddb06dd933214e7beb7c56dc21822e0bf
Patrice Cadiot <pathibul.r@gmail.com>
2017-10-28 13:17:25 +0200

    crée dossier views

--- base_controller.rb
+++ base_controller.rb
@@ -3,7 +3,7 @@ class BaseController
   private
 
   def render(filename)
-    status, body = Renderer.new(filename).render
+    status, body = Renderer.new(File.join('views', filename)).render
     [status, {}, [body]]
   end
 end
